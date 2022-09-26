7259f403d3d8e54084a2c1e0a27381918ab5d62a
Filip <filip.fabris@fer.hr>
2022-09-26 11:45:20 +0200

    first commit

---
+++ main.tf
@@ -0,0 +1,75 @@
+provider "aws" {
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
+    region = var.region
+}
+
+#Create aws security group
+resource "aws_security_group" "test_security_group" {
+    name = "nova"
+    description = "Created via Terraform"
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+#Create aws instance for haproxy
+resource "aws_instance" "haproxy_load_balancer" {
+    security_groups = ["${aws_security_group.test_security_group.name}"]
+    instance_type = "${var.load_balancer_instance_type}"
+    key_name = "${var.aws_key_name}"
+    ami = "${var.load_balancer_ami}"
+
+    # The connection block tells our provisioner how to
+    # communicate with the resource (instance)
+    connection {
+        user = "ec2-user"
+        private_key = "${var.aws_pem_key_file_path}"
+        host = self.public_ip
+    }
+
+    count = 1
+
+    # provisioner "file" {
+    #     source = "tf-remote-haproxy-script.sh"
+    #     destination = "/tmp/tf-remote-haproxy-script.sh"
+    # }
+
+    # provisioner "remote-exec" {
+    #     # some `sleep` might be needed to prevent i/o timeout here?
+    #     inline = [
+    #       "sleep 420",
+    #       "chmod +x /tmp/tf-remote-haproxy-script.sh",
+    #       "sudo /tmp/tf-remote-haproxy-script.sh"
+    #     ]
+    # }
+}
+
+#Create aws instance for apache web
+resource "aws_instance" "web" {
+    security_groups = ["${aws_security_group.test_security_group.name}"]
+    key_name = "${var.aws_key_name}"
+    instance_type = "${var.web_instance_type}"
+
+    ami = "${var.web_ami}"
+
+    connection {
+        user = "ec2-user"  #Za OS instancu je drugaciji default user, nije moguc root login
+        private_key = file(var.aws_pem_key_file_path)
+        host = self.public_ip
+    }
+
+    #count = "${var.desired_web_server_count}"
+    #count = 0
+}
---
+++ terraform.tfvars
@@ -0,0 +1,9 @@
+aws_pem_key_file_path = "./my_aws.pem"
+aws_key_name = "my_aws"
+
+#################
+# SECRET KEYS!! #
+#################
+
+aws_access_key = "AjjAhhC6ggAzzFyy7Lxx"  #Key ID
+aws_secret_key = "dmjjjqbiiiGJKUeeeYidddb26cccEwJqbbbDTaaa"  #Key secret
